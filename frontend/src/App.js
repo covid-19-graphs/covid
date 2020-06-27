6a9b87655cc0394aa923838f195dbdeefbaf652d
Jim Hall <jim.hall.dev@gmail.com>
2020-06-27 13:24:20 -0400

    add image copying to the build

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -21,14 +21,12 @@ export default function App() {
           </ul>
         </nav>
 
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
         <Switch>
-          <Route path="/about">
-            <About />
-          </Route>
           <Route path="/">
             <Home />
+          </Route>
+          <Route path="/about">
+            <About />
           </Route>
         </Switch>
       </div>
